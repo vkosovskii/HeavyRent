f0cf0d869e62e9f00eebdaa58f94d8d032a7a652
kosovskii <kosovskii@rushstreetinteractive.com>
2026-03-04 00:03:54 +0200

    add new grpc base

--- user-service/src/main/java/com/heavyrent/user/grpc/UserGrpcServiceImpl.java
+++ user-service/src/main/java/com/heavyrent/user/grpc/UserGrpcServiceImpl.java
@@ -1,6 +1,8 @@
 package com.heavyrent.user.grpc;
 
 import com.google.protobuf.Timestamp;
+import com.heavyrent.grpc.common.UserContext;
+import com.heavyrent.grpc.common.UserContextHolder;
 import com.heavyrent.grpc.user.*;
 import com.heavyrent.user.dto.UserProfileResponse;
 import com.heavyrent.user.service.UserProfileService;
@@ -16,7 +18,7 @@ import java.util.UUID;
 @GrpcService
 public class UserGrpcServiceImpl extends UserGrpcServiceGrpc.UserGrpcServiceImplBase {
 
-    UserProfileService profileService;
+    private final UserProfileService profileService;
 
     public UserGrpcServiceImpl(UserProfileService service) {
         this.profileService = service;
@@ -30,14 +32,45 @@ public class UserGrpcServiceImpl extends UserGrpcServiceGrpc.UserGrpcServiceImpl
             responseObserver.onNext(response);
             responseObserver.onCompleted();
         } catch (NoSuchElementException e) {
+            responseObserver
+                    .onError(Status.NOT_FOUND
+                            .withDescription("User not found with public ID: " + request.getPublicId())
+                            .asRuntimeException());
+        }
+    }
+
+    @Override
+    public void getUserByKeycloakId(GetUserByKeycloakIdRequest request, StreamObserver<UserGrpcResponse> responseObserver) {
+        try {
+            UUID keycloakId = UUID.fromString(request.getKeycloakId());
+            UserGrpcResponse response = toGrpcResponse(profileService.getUserByKeycloakId(keycloakId));
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (NoSuchElementException e) {
             responseObserver.onError(Status.NOT_FOUND
-                    .withDescription("User not found with public ID: " + request.getPublicId())
+                    .withDescription("User not found with keycloak ID: " + request.getKeycloakId())
                     .asRuntimeException());
+        }
+    }
+
+    @Override
+    public void getCurrentUser(GetCurrentUserRequest request, StreamObserver<UserGrpcResponse> responseObserver) {
+        try {
+            UserContext ctx = UserContextHolder.KEY.get();
+            UUID keycloakId = UUID.fromString(ctx.keycloakId());
+            UserGrpcResponse response = toGrpcResponse(profileService.getUserByKeycloakId(keycloakId));
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (NoSuchElementException e) {
+            responseObserver.onError(
+                    Status.NOT_FOUND.withDescription("User not found with keycloak")
+                            .asRuntimeException());
         }
     }
 
     private UserGrpcResponse toGrpcResponse(UserProfileResponse user) {
         return UserGrpcResponse.newBuilder()
+                .setKeycloakId(user.keycloakId().toString())
                 .setPublicId(user.publicId().toString())
                 .setEmail(user.email())
                 .setFirstName(user.firstName())
@@ -54,11 +87,7 @@ public class UserGrpcServiceImpl extends UserGrpcServiceGrpc.UserGrpcServiceImpl
         if (dateTime == null) {
             return null;
         }
-
-        return Timestamp.newBuilder()
-                .setSeconds(dateTime.toEpochSecond(ZoneOffset.UTC))
-                .setNanos(dateTime.getNano())
-                .build();
+        return Timestamp.newBuilder().setSeconds(dateTime.toEpochSecond(ZoneOffset.UTC)).setNanos(dateTime.getNano()).build();
     }
 }
 
