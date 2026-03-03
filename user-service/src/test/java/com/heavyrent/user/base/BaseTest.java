4c004155637aa6268eb29bf98f7c64e295b27c35
kosovskii <kosovskii@rushstreetinteractive.com>
2026-03-04 00:02:38 +0200

    add test data

---
+++ user-service/src/test/java/com/heavyrent/user/base/BaseTest.java
@@ -0,0 +1,8 @@
+package com.heavyrent.user.base;
+
+import java.util.UUID;
+
+public abstract class BaseTest {
+
+    public UUID keycloakId = UUID.fromString("c0bb46fd-d102-49fc-b293-bd94a1be3705");
+}
