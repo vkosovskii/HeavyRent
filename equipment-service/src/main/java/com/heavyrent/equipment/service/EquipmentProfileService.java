36943e34208bb2ae534162e75cf15fc07bc54162
kosovskii <kosovskii@rushstreetinteractive.com>
2026-03-07 20:56:54 +0200

    add findAll and add logic to createEquipmentProfile

--- equipment-service/src/main/java/com/heavyrent/equipment/service/EquipmentProfileService.java
+++ equipment-service/src/main/java/com/heavyrent/equipment/service/EquipmentProfileService.java
@@ -1,15 +1,22 @@
 package com.heavyrent.equipment.service;
 
+import com.heavyrent.equipment.dto.EquipmentFilterRequest;
 import com.heavyrent.equipment.dto.EquipmentProfileRequest;
 import com.heavyrent.equipment.dto.EquipmentProfileResponse;
 import com.heavyrent.equipment.model.EquipmentProfile;
 import com.heavyrent.equipment.repository.EquipmentProfileRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+
+import static com.heavyrent.equipment.specification.EquipmentSpecification.*;
 
 @Service
 @Slf4j
@@ -21,10 +28,12 @@ public class EquipmentProfileService {
         this.equipmentProfileRepository = equipmentProfileRepository;
     }
 
-    public List<EquipmentProfileResponse> findByOwnerId(UUID ownerId) {
-        log.info("Finding equipment by ownerId: {}", ownerId);
-        List<EquipmentProfile> equipmentProfiles = equipmentProfileRepository.findByOwnerId(ownerId);
-        log.info("Found {} equipment profiles for ownerId: {}", equipmentProfiles.size(), ownerId);
+    /// For inter-service use only!!
+    /// This key should not be shown to the client.
+    public List<EquipmentProfileResponse> findByOwnerKeycloakId(UUID ownerKeycloakId) {
+        log.info("Finding equipment by owner Keycloak ID: {}", ownerKeycloakId);
+        List<EquipmentProfile> equipmentProfiles = equipmentProfileRepository.findByOwnerKeycloakId(ownerKeycloakId);
+        log.info("Found {} equipment profiles for ownerId: {}", equipmentProfiles.size(), ownerKeycloakId);
         return equipmentProfiles.stream()
                 .map(this::toResponse)
                 .toList();
@@ -36,10 +45,11 @@ public class EquipmentProfileService {
                 .orElseThrow(() -> new NoSuchElementException("Equipment not found: " + equipmentId)));
     }
 
-    public EquipmentProfileResponse createEquipmentProfile(EquipmentProfileRequest request, UUID ownerId) {
-        log.info("Creating equipment profile for ownerId: {}", ownerId);
+    public EquipmentProfileResponse createEquipmentProfile(EquipmentProfileRequest request, UUID ownerKeycloakId, UUID ownerPublicId) {
+        log.info("Creating equipment profile for ownerId: {}", ownerPublicId);
         EquipmentProfile profile = new EquipmentProfile();
-        profile.setOwnerId(ownerId);
+        profile.setOwnerKeycloakId(ownerKeycloakId);
+        profile.setOwnerPublicId(ownerPublicId);
         EquipmentProfileResponse response = toResponse(equipmentProfileRepository.save(fillInEquipmentProfile(profile, request)));
         log.info("Created equipment profile with id: {}", response.publicId());
         return response;
@@ -56,6 +66,20 @@ public class EquipmentProfileService {
         log.info("Deleting equipment profile with id: {}", equipmentId);
         equipmentProfileRepository.deleteByPublicId(equipmentId);
         log.info("Deleted equipment profile with id: {}", equipmentId);
+    }
+
+    public Page<EquipmentProfileResponse> findAll(EquipmentFilterRequest filter, int page, int pageSize) {
+        log.info("Finding all equipment profiles");
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Specification<EquipmentProfile> spec = Specification
+                .where(byOwnerPublicId(filter.ownerPublicId()))
+                .and(byName(filter.name()))
+                .and(byModel(filter.model()))
+                .and(byType(filter.type()))
+                .and(byStatus(filter.equipmentStatus()))
+                .and(byMaxPrice(filter.maxPricePerHourCents()));
+
+        return equipmentProfileRepository.findAll(spec, pageable).map(this::toResponse);
     }
 
     private EquipmentProfile fillInEquipmentProfile(EquipmentProfile profile, EquipmentProfileRequest equipmentProfileResponse) {
