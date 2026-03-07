7fdc6f95f0367acd214465b86d218551d18d41e6
kosovskii <kosovskii@rushstreetinteractive.com>
2026-03-07 20:57:40 +0200

    Create gRPC client

---
+++ equipment-service/src/main/java/com/heavyrent/equipment/grpc/UserServiceClient.java
@@ -0,0 +1,35 @@
+package com.heavyrent.equipment.grpc;
+
+
+import com.heavyrent.grpc.user.GetUserByKeycloakIdRequest;
+import com.heavyrent.grpc.user.UserGrpcResponse;
+import com.heavyrent.grpc.user.UserGrpcServiceGrpc;
+import io.grpc.StatusRuntimeException;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class UserServiceClient {
+
+    @GrpcClient("user-service")
+    UserGrpcServiceGrpc.UserGrpcServiceBlockingStub stub;
+
+    public UUID getPublicIdBy(UUID keycloakId) {
+        log.info("Get public id by {}", keycloakId);
+        GetUserByKeycloakIdRequest request = GetUserByKeycloakIdRequest.newBuilder()
+                .setKeycloakId(String.valueOf(keycloakId))
+                .build();
+        UserGrpcResponse response;
+        try {
+            response = stub.getUserByKeycloakId(request);
+        } catch (StatusRuntimeException e) {
+            log.error("RPC failed: {}", e.getStatus());
+            throw new RuntimeException(e);
+        }
+        return UUID.fromString(response.getPublicId());
+    }
+}
